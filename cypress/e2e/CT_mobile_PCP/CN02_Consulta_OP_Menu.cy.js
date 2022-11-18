567cad278807b97c4cf92272e8e84050f2b69c9e
Murilo <murilo.souza@totvs.com.br>
2022-11-18 10:42:58 -0300

    Teste 04

---
+++ cypress/e2e/CT_mobile_PCP/CN02_Consulta_OP_Menu.cy.js
@@ -0,0 +1,75 @@
+///<reference types="cypress" />
+
+// AUTOR........: Murilo Beckhauser de Souza;
+// DATA.........: 25/03/2022;
+// INFORMAÇÕES..: O Objetivo deste teste é realizar a consulta de OP através do Menu Lateral " Consulta de O.P."
+// 1 - O cenário clica em Consulta de O.P / Clica em Busca Avançada / Informa o item MBS001-a e a data entre 01/01/2018 à 31/12/2021 /
+// clica em Aplicar / Na OP 858 clica em Detalhar / clica na seta Voltar, no cabeçalho da tela / clica no botão LIMPAR / clica na seta
+// Voltar / clica novamente no Menu / Clica novamente em Consulta de O.P / Clica no "X", onda vai eliminar a OP 858 / Informa a OP 872
+// no campo de pesquisa e clica na Lupa / Na OP 872 clica em Detalhar / clica na seta Voltar, no cabeçalho da tela / clica na seta Voltar,
+// no cabeçalho da tela novamente / Clica novamente no Menu / Clica novamente em Consulta de O.P / Em últimas Ordens Consultadas, clica na
+// OP 872 / clica em Apontamentos / Após apresentas os apontamentos, clica na Seta Volta no cabeçalho da tela.
+
+describe('app_minha_producao', () => {
+
+  it('Consulta de OP através do Menu', () => {
+
+    cy.tamanhoTelaParaMobile()
+    cy.Config() //esta customização está em commands
+    cy.Login()  //esta customização está em commands
+
+    cy.intercept('POST', '**/api/cpp/v1/productionMobile/**').as('postMenu')
+    cy.get('ion-menu-button').click() //Clica no botão Menu
+    cy.get('.center').click() //Clica no logo da Totvs
+    cy.contains('Consulta de O.P.').click()
+    cy.contains('Busca Avançada').click()
+    cy.wait('@postMenu')
+    cy.get('input[name*="itemCode"]').type('mbs001-a')
+    cy.get('input[name*="startDateRange"]').type('{selectall}01012018')
+    cy.get('input[name*="endDateRange"]').type('{selectall}31122021')
+    cy.contains('Aplicar').click()
+    cy.contains('858').click()
+    cy.get('.ng-star-inserted.ion-page > .header-md > .toolbar-title-default > .buttons-first-slot > .md').click() //Clica na seta de Voltar no cabeçalho
+    cy.contains('Limpar').click()
+    cy.get('app-production-query-datasul.ion-page > .header-md > .toolbar-title-default > .buttons-first-slot > .md').click() //Clica na seta de Voltar no cabeçalho
+    cy.get('app-form-list-datasul.ion-page > .header-md > .toolbar-title-default > .buttons-last-slot > .button').click() //Clica na Seta de Atualizar no cabeçalho
+    cy.get('ion-menu-button').click() //Clica no botão Menu
+    cy.get('.center').click() //Clica no logo da Totvs
+    cy.contains('Consulta de O.P.').click()
+    cy.get('.icon-red').click() //Clica no "X", para deletar a OP 858
+    cy.wait('@postMenu')
+    cy.get('input[placeholder*="Informe a OP ou o Item"]').type('872')
+    cy.get('po-icon[ng-reflect-icon*="po-icon-search"]').click() //Clica na Lupa
+    cy.contains('Detalhar').click()
+    cy.get('app-production-order-detail.ng-star-inserted > .header-md > .toolbar-title-default > .buttons-first-slot > .md').click() //Clica na seta de Voltar no cabeçalho
+    cy.contains('Limpar').click()
+    cy.get('.ng-star-inserted.ion-page > .header-md > .toolbar-title-default > .buttons-first-slot > .md').click() //Clica na seta de Voltar no cabeçalho
+    cy.get('app-form-list-datasul.ion-page > .header-md > .toolbar-title-default > .buttons-last-slot > .button').click() //Clica na Seta de Atualizar no cabeçalho
+    cy.get('ion-menu-button').click() //Clica no botão Menu
+    cy.get('.center').click() //Clica no logo da Totvs
+    cy.wait('@postMenu')
+    cy.contains('Consulta de O.P.').click()
+    cy.contains('MBS002-a').click()
+    cy.intercept('GET', '**/joisrvapldev001:8180/api/cpp/**').as('getApontamentos')
+    cy.contains('Visualização Resumida').click()
+    cy.get('po-info').should('contain', 'Item: MBS002-a')
+    cy.get('po-info').should('contain', 'Qtde Prevista: 100,0000')
+    cy.get('po-info').should('contain', 'Qtde Prod: 90,0000')
+    cy.get('po-info').should('contain', 'Qtde Refug: 9,0000')
+    cy.get('po-info').should('contain', 'Início: 16/05/2019')
+    cy.get('po-info').should('contain', 'Unidade de Negócio: 99')
+    cy.get('po-info').should('contain', 'Estabelecimento: MBS')
+    cy.get('po-info').should('contain', 'Lote/Série: MBS.000872')
+    cy.get('po-info').should('contain', 'Referência: Azul')
+    cy.get('po-info').should('contain', 'Planejador: Murilo')
+    cy.contains('po-button', 'Apontamentos').click()
+    //cy.wait('@getApontamentos').its('response.body').should('property','total', 3)
+    cy.get('po-info').should('contain', 'Qtde Refug: 2,0000')
+    cy.get('po-info').should('contain', 'Qtde Prod: 20,0000')
+    cy.get('po-info').should('contain', 'Qtde Refug: 3,0000')
+    cy.get('po-info').should('contain', 'Qtde Prod: 30,0000')
+    cy.get('po-info').should('contain', 'Qtde Refug: 4,0000')
+    cy.get('po-info').should('contain', 'Qtde Prod: 40,0000')
+    cy.get('app-production-order-reports.ng-star-inserted > .header-md > .toolbar-title-default > .buttons-first-slot > .md').click()
+  })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,119 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+///<reference types="cypress" />
+
+Cypress.Commands.add('tamanhoTelaParaMobile', () => {
+    cy.viewport(360, 760)
+  })
+  
+  Cypress.Commands.add('Config', () =>{
+  
+      cy.intercept('GET', 'http://localhost:8100/index.html').as('getConfig')
+      cy.visit('http://localhost:8100/index.html', { force: true })
+      cy.get('div').then((nome_qualquer) => {
+        if (nome_qualquer.find('div', 'Carregando')) {
+          cy.visit('http://localhost:8100/index.html',{ force: true })
+        }
+        else {
+          cy.contains('div', 'Olá!').click({ force: true })
+        }
+      })
+  
+      cy.contains('div', 'Olá!').click({ force: true })
+      cy.contains('ion-button', 'Começar a utilizar o Minha Produção').click()
+      cy.contains('ion-button', 'Aceitar ').click()
+      cy.contains('ion-button', 'Editar').click()
+      cy.get('ion-radio').first().click()
+      cy.get('ion-radio[ng-reflect-value=4]').click()
+      cy.get('input[placeholder="Configurações"]').type('joisrvapldev001')
+      cy.get('input[placeholder="Porta"]').type('8180')
+      cy.contains('ion-button', 'Salvar').click()
+      cy.wait('@getConfig').its('response.statusCode').should('eq', 200)
+      
+    })
+    
+    
+    Cypress.Commands.add('Login', () => {
+    
+      const usuario = 'super' //variável
+      const senha = 'Super@123'     //variável
+      cy.intercept('GET', '**/dts/datasul-rest/resources/**').as('getLogar')
+      cy.get('input[placeholder*="Seu usuário"]').type(usuario)
+      cy.get('input[placeholder*="Sua senha"]').type((senha), { log: false })
+      cy.contains('button', 'Entrar').click()
+      cy.wait('@getLogar').its('response.statusCode').should('eq', 200)
+    
+    })
+    
+    
+    Cypress.Commands.add('loginAptoHtml', () => {
+    
+      cy.visit('http://joisrvapldev001:8180/totvs-menu/#/', {force: true})
+      cy.get('#txtUsername').type('super')
+      cy.get('#txtPassword').type('Super@123')
+      cy.contains('Entrar').click()
+      cy.visit('http://joisrvapldev001:8180/totvs-menu/#/dts/mcp/productionappointment/', {force: true})
+    
+    })
+  
+    
+    Cypress.Commands.add('mostraParadasDeMaquina', () => {
+      //Esta customização pertence aos cenários: CN06_Consulta_OP_Operac_e_Paradas_SFC.cy
+      cy.contains('ion-title', 'Máquina MBS').should('be.visible')
+      cy.get('div').should('contain',' Paradas ')
+      cy.intercept('GET', '**/joisrvapldev001:8180/api/sfc/v1/stops/**').as('paradas')
+  
+      cy.get('po-field-container[ng-reflect-label="Início"]').type('{selectall}01012018')
+      cy.get('po-field-container[ng-reflect-label="Término"]').type('{selectall}10/07/2022')            
+      cy.contains(' Paradas ').click()    
+  
+      cy.get('po-info').should('contain', 'Tempo: 1,00 h')
+      cy.get('po-info').should('contain', 'Início: 13/03/2018 - 08:00').and('contain', 'Término: 13/03/2018 - 09:00')
+      cy.get('po-info').should('contain', 'Motivo: 001 - Troca de Ferramenta')
+      
+      cy.get('po-info').should('contain', 'Tempo: 1,00 h')
+      cy.get('po-info').should('contain', 'Início: 05/07/2022 - 06:30').and('contain', 'Término: 05/07/2022 - 07:30')
+      cy.get('po-info').should('contain', 'Motivo: 001 - Troca de Ferramenta')
+  
+      cy.get('po-info').should('contain', 'Tempo: 1,00 h')
+      cy.get('po-info').should('contain', 'Início: 05/07/2022 - 09:00').and('contain', 'Término: 05/07/2022 - 10:30')
+      cy.get('po-info').should('contain', 'Motivo: 003 - Resfriamento da solda')
+  
+      cy.get('po-info').should('contain', 'Tempo: 1,25 h')
+      cy.get('po-info').should('contain', 'Início: 05/07/2022 - 16:00').and('contain', 'Término: 05/07/2022 - 17:15')
+      cy.get('po-info').should('contain', 'Motivo: 002 - Manutenção Preventiva')
+   
+      cy.get('ion-toast').click({force: true})        
+        .invoke('attr', 'style', 'visibility:hidden')
+      cy.wait('@paradas')
+  
+    })
+  
+    Cypress.Commands.add('clicaSetaVoltarParaInicioFormulario', () => {
+      //Esta customização pertence aos cenários: CN06_Consulta_OP_Operac_e_Paradas_SFC.cy
+      cy.get('app-production-order-stops.ng-star-inserted > .header-md > .toolbar-title-default > .buttons-first-slot > .md').click()
+      cy.get('app-production-order-reports.ng-star-inserted > .header-md > .toolbar-title-default > .buttons-first-slot > .md').click()
+      cy.contains(' Visualização Resumida ').click()
+      cy.get('.ng-star-inserted.ion-page > .header-md > .toolbar-title-default > .buttons-first-slot > .md').click()
+      cy.get('app-production-query-datasul.ion-page > .header-md > .toolbar-title-default > .buttons-first-slot > .md').click()
+  
+    })
+  
+    Cypress.Commands.add('incluirNovaReserva', () => {
+      //Esta customização pertence ao cenário: CN06_Botoes_Aba_Reservas_Narrativa_PCP.cy
+      cy.get('po-select[name="referenceCode"]').click()
+      cy.contains('span','AZUL - AZUL').click()
+      cy.get('input[name="operationCode"]').clear().type('10')
+      cy.get('input[name="lotSeries"]').type('Lote Teste')
+      cy.get('input[name="materialDate"]').type('{selectall}02092022')
+    })
+  
+    Cypress.Commands.add('dataHora', () => {
+      //Esta customização pertence ao cenário: CN07_Apont_OP_3_operac_1_externa.cy.js
+      cy.get('input[name="dat_inic_setup"]').clear().type('09092022')
+      cy.get('input[name="qtd_segs_inic_setup"]').clear().type('0430')
+      cy.get('input[name="dat_fim_setup"]').clear().type('09092022')
+      cy.get('input[name="qtd_segs_fim_setup"]').clear().type('0530')
+      cy.get('input[name="dat_inic_reporte"]').clear().type('09092022')
+      cy.get('input[name="qtd_segs_inic_reporte"]').clear().type('0530')
+      cy.get('input[name="dat_fim_reporte"]').clear().type('09092022')
+      cy.get('input[name="qtd_segs_fim_reporte"]').clear().type('0750')
+    })
